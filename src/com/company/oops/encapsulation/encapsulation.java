4414d3d91e3b94d56d36f9d036a1bc6b443bc140
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-07-04 23:35:39 +0530

    done with Encapsulation.

---
+++ src/com/company/oops/encapsulation/encapsulation.java
@@ -0,0 +1,13 @@
+package com.company.oops.encapsulation;
+
+public class encapsulation {
+    public static void main(String[] args) {
+        System.out.println("Encapsuation focus on internall data hiding.");
+    }
+
+    public static void abstraction() {
+        System.out.println("Absttraction deals with the external data hiding." + "\n" + "That is it is matter of desgin ");
+    }
+
+}
+
