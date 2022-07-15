7da8d7f4f4b743e6b78c83e9733af2190ab352ef
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-07-15 12:06:49 +0530

    Done with nested interface

--- src/com/company/nestedInterface/A.java
+++ src/com/company/nestedInterface/A.java
@@ -1,4 +1,15 @@
 package com.company.nestedInterface;
-
+//program for ood or not
 public class A {
+    public interface NestedInterface {
+        boolean isOdd(int num);
+    }
+}
+
+class B implements A.NestedInterface {
+
+    @Override
+    public boolean isOdd(int num) {
+        return (num & 1) == 1;
+    }
 }
---
+++ src/com/company/nestedInterface/Main.java
@@ -0,0 +1,8 @@
+package com.company.nestedInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.isOdd(13));
+    }
+}
